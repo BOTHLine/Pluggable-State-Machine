0bb7598f23d6723c653be1135e35b5b19df78c0e
BOTHLine <42834479+BOTHLine@users.noreply.github.com>
2018-10-16 16:22:06 +0200

    Trying State Machine and Behaviour

--- Unity Project/Assets/Scripts/Scriptable Objects/Variables/_Base/BaseVariable.cs
+++ Unity Project/Assets/Scripts/Scriptable Objects/Variables/_Base/BaseVariable.cs
@@ -3,7 +3,21 @@
 public abstract class BaseVariable<TDatatype> : ScriptableObject, ISerializationCallbackReceiver
 {
     public TDatatype InitialValue;
-    public TDatatype Value;
+    private TDatatype _Value;
+    public TDatatype Value
+    {
+        get
+        {
+            return _Value;
+        }
+        set
+        {
+            _Value = value;
+            OnValueChange.Invoke(_Value);
+        }
+    }
+
+    public System.Action<TDatatype> OnValueChange = delegate { };
 
     public void SetValue(TDatatype value) { Value = value; }
     public void SetValue(BaseVariable<TDatatype> value) { Value = value.Value; }
